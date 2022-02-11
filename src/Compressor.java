e527009364e5ef00fd9e941af9a398e5d2f0fafa
kelle1ds <kelle1ds@cmich.edu>
2022-02-11 18:57:54 -0500

    Lots of work.  Up to HashMap

--- src/Compressor.java
+++ src/Compressor.java
@@ -3,7 +3,10 @@ import java.io.FileNotFoundException;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Compressor {
 
@@ -16,7 +19,7 @@ public class Compressor {
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
-		
+
 		Compressor c = new Compressor(fileContents);
 		c.printSymbolCodeMap();
 		
@@ -32,16 +35,55 @@ public class Compressor {
 	//  symbol frequencies in the fileContents provided.
 	// Chunk through the file win lengths of SYMBOL_LENGTH;
 	Compressor(String fileContents) {
-		
-		// Hints: 
-		
-		// Determine symbol frequencies
-		
+
+		PriorityQueue<HashMap> pQueue = new PriorityQueue<>();
+		ArrayList<String> list = new ArrayList<>();
+
+		int length = fileContents.length();
+		int reminder = length % SYMBOL_LENGTH;  //just in case input is not / 8
+
+		for(int i = 0; i < fileContents.length()-reminder; i = i + SYMBOL_LENGTH){
+
+			//System.out.println(fileContents.substring(i,i+SYMBOL_LENGTH));  //test print
+			list.add(fileContents.substring(i,i+SYMBOL_LENGTH)); //add 8 char substring to list
+		}
+
+		// create Hashmap for frequency counts
+		HashMap map = countFrequencies(list);
+
+		//map.entrySet();
+
+		Iterator iterator = map.entrySet().iterator();
+		while (iterator.hasNext()) {
+			Map.Entry me2 = (Map.Entry) iterator.next();
+			System.out.println("Binary value: "+me2.getKey() + " & Frequency: " + me2.getValue());
+		}
+
 		// Build code tree
 		
 		// Create encoding map
 		
 		
+	}
+
+	//Hashmap function to create the hashmap for the ArrayList
+	public static HashMap countFrequencies(ArrayList<String> list)
+	{
+		// hashmap to store the frequency of element
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+		for (String sub : list) {
+			Integer j = map.get(sub);
+			map.put(sub, (j == null) ? 1 : j + 1);
+		}
+
+		// Display frequency while creating.  Remove before submission
+		/*for (HashMap.Entry<String, Integer> val : map.entrySet()) {
+			System.out.println("Element " + val.getKey() + " "
+					+ "occurs"
+					+ ": " + val.getValue() + " times");
+		}*/
+		return map;
 	}
 	
 	//  Prints out each symbol with its code
